d2f1159163d8ed32c19a48b77fa076b67e8f1cc7
ggrandes <guillermo.grandes@gmail.com>
2025-08-19 17:47:34 +0200

    Migrate to jakarta.servlet-api (tomcat 10+)

--- src/main/java/org/javastack/test/TestServlet.java
+++ src/main/java/org/javastack/test/TestServlet.java
@@ -2,11 +2,11 @@ package org.javastack.test;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public final class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 42L;
